e635b35cb99cbf22f0588af32c57a3bd32269d4b
yukkun007 <kato.yutaka@gmail.com>
2020-02-07 06:11:52 +0900

    メソッド名を変更

--- mmlibrary/html_page.py
+++ mmlibrary/html_page.py
@@ -30,45 +30,45 @@ class HtmlPage:
         self.driver = Chrome(options=options)
         logging.debug("driver.create/end")
 
-    def __wait(self) -> None:
+    def _wait(self) -> None:
         WebDriverWait(self.driver, 10, poll_frequency=0.05).until(
             ec.presence_of_all_elements_located
         )
 
-    def __wait_element(self, target):
+    def _wait_element(self, target):
         element = WebDriverWait(self.driver, 10, poll_frequency=0.05).until(
             ec.presence_of_element_located(target)
         )
 
         return element
 
-    def __login(self, login_url: str, user: User) -> None:
+    def _login(self, login_url: str, user: User) -> None:
         logging.debug("[0] login start. : user.name={}, url={}".format(user.name, login_url))
 
         self.driver.get(login_url)
         logging.debug("[1] driver.get done.")
 
-        self.__wait()
+        self._wait()
         logging.debug("[2] wait done.")
 
         # ログインボタン押下
-        uid = self.__wait_element((By.NAME, "usercardno"))
-        password = self.__wait_element((By.NAME, "userpasswd"))
+        uid = self._wait_element((By.NAME, "usercardno"))
+        password = self._wait_element((By.NAME, "userpasswd"))
         uid.send_keys(user.id)
         password.send_keys(user.password)
         logging.debug("[3] send keys done.")
-        button = self.__wait_element((By.NAME, "Login"))
+        button = self._wait_element((By.NAME, "Login"))
         logging.debug("[4] button element located done.")
         button.click()
         logging.debug("[5] button click done.")
 
         # ロードされたかを確認
-        self.__wait_element((By.NAME, "FormLEND"))
+        self._wait_element((By.NAME, "FormLEND"))
         logging.debug("[6] load all wait done.")
         logging.debug("[7] login end.")
 
     def fetch_login_page(self, login_url: str, user: User) -> str:
-        self.__login(login_url, user)
+        self._login(login_url, user)
         logging.debug("driver.page_source.encode/start")
         html = self.driver.page_source.encode("utf-8")
         logging.debug("driver.page_source.encode/end")
