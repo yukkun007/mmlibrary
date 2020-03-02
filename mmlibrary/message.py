7c928a2ff69fa7cfb8b39818d8d1edf4e31c8b1f
yukkun007 <kato.yutaka@gmail.com>
2020-03-02 15:03:59 +0900

    テンプレートを相対パスで読み込むように変更

--- mmlibrary/message.py
+++ mmlibrary/message.py
@@ -1,13 +1,17 @@
+import os
+from pathlib import Path
 from jinja2 import Environment, FileSystemLoader
 
 
 class Message(object):
-
-    TEMPLATE_ROOT_DIR = "mmlibrary/template"
+    @staticmethod
+    def _get_template_root_dir() -> str:
+        path = Path(os.path.abspath(__file__))
+        return str(path.parent.joinpath("template"))
 
     @staticmethod
     def create(template_path, data) -> str:
-        env = Environment(loader=FileSystemLoader(Message.TEMPLATE_ROOT_DIR))
+        env = Environment(loader=FileSystemLoader(Message._get_template_root_dir()))
         template = env.get_template(template_path)
         message = template.render(data)
         return message
