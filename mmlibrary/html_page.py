8247928c5acc840b7abb16e00a8993a5d1dbbff9
yukkun007 <kato.yutaka@gmail.com>
2020-03-08 10:36:49 +0900

    test

--- mmlibrary/html_page.py
+++ mmlibrary/html_page.py
@@ -26,6 +26,7 @@ class HtmlPage:
         # 必須
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")  # Bypass OS security model
+        options.add_argument("--single-process")
         # options.add_argument("--disable-setuid-sandbox")
         options.add_argument("--disable-gpu")  # applicable to windows os only
         # エラーの許容
